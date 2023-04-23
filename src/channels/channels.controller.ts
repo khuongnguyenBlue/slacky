19a2352953b86809d9f3e9ae62dabc5497829daf
Kay Nguyen <kay.nguyen@shopback.com>
2023-04-23 11:50:26 +0700

    API create channel

--- src/channels/channels.controller.ts
+++ src/channels/channels.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UserContext } from 'src/global/contexts/user.context';
 import { ChannelsService } from './channels.service';
+import { CreateChannelDto } from './channels.dtos';
 
 @Controller(':clientToken/channels')
 export class ChannelsController {
@@ -14,5 +15,12 @@ export class ChannelsController {
     const userId = this.userContext.getUserId();
     const channels = await this.channelsService.findChannelsByMemberId(userId);
     return { channels };
+  }
+
+  @Post()
+  async createChannel(@Body() body: CreateChannelDto) {
+    const userId = this.userContext.getUserId();
+    const channel = await this.channelsService.createChannel(userId, body);
+    return { channel };
   }
 }
--- src/channels/channels.service.spec.ts
+++ src/channels/channels.service.spec.ts
@@ -2,11 +2,16 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { ChannelsService } from './channels.service';
 import { ChannelsRepository } from './channels.repository';
 import { ChannelType } from '@prisma/client';
+import { CreateChannelDto } from './channels.dtos';
+import { BadRequestException } from '@nestjs/common';
+import { rejects } from 'assert';
 
 describe('ChannelsService', () => {
   let service: ChannelsService;
   const repository = {
     findChannelsByMemberId: jest.fn(),
+    findChannelByName: jest.fn(),
+    createChannel: jest.fn(),
   };
   const userId = 1;
 
@@ -58,6 +63,55 @@ describe('ChannelsService', () => {
         const result = await service.findChannelsByMemberId(userId);
         expect(repository.findChannelsByMemberId).toBeCalledWith(userId);
         expect(result).toEqual(mockChannels);
+      });
+    });
+  });
+
+  describe('createChannel', () => {
+    const mockChannel = {
+      id: 1,
+      workspaceId: 1,
+      creatorId: 1,
+      name: 'general',
+      type: ChannelType.PUBLIC,
+      archived: false,
+      description: 'Non quia ut quas.',
+      createdAt: new Date(),
+    };
+
+    const body: CreateChannelDto = {
+      workspaceId: 1,
+      name: 'learning',
+      type: 'PUBLIC',
+    };
+
+    describe('happy case', () => {
+      beforeEach(() => {
+        repository.findChannelByName.mockResolvedValue(null);
+        repository.createChannel.mockResolvedValue(mockChannel);
+      });
+
+      it('should create new channel correctly', async () => {
+        const result = await service.createChannel(userId, body);
+        expect(repository.findChannelByName).toBeCalledWith(body.name);
+        expect(repository.createChannel).toBeCalledWith(userId, body);
+        expect(result).toEqual(mockChannel);
+      });
+    });
+
+    describe('validation', () => {
+      // TODO
+    });
+
+    describe('when channel name has been taken', () => {
+      beforeEach(() => {
+        repository.findChannelByName.mockResolvedValue(mockChannel);
+      });
+
+      it('should throw bad request', async () => {
+        expect(service.createChannel(userId, body)).rejects.toThrowError(
+          BadRequestException,
+        );
       });
     });
   });
