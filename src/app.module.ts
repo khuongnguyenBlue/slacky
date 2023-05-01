79ee4df301ab1ecf8e2e7ef7d7d9336222d495d1
Kay Nguyen <kay.nguyen@shopback.com>
2023-05-01 11:00:05 +0700

    Change API response structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
-import { SeederModule } from './seeder/seeder.module';
 import { ChannelsModule } from './channels/channels.module';
 import { ClientTokenMiddleware } from './middlewares/client-token.middleware';
 import { GlobalModule } from './global/global.module';
@@ -12,7 +11,6 @@ import configuration from './config/configuration';
 @Module({
   imports: [
     PrismaModule,
-    SeederModule,
     ChannelsModule,
     GlobalModule,
     ConfigModule.forRoot({
--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-
+  app.enableCors();
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
   await app.listen(port);
